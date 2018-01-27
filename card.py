00c4cb6083fd3fa940249613042c6f008c525b25
Metamess <metamess@gmail.com>
2018-01-27 02:44:42 +0100

    Changed order of suits in Card to be alternating black/red

--- card.py
+++ card.py
@@ -1,7 +1,7 @@
 import functools
 
-suit_symbols = ["♠", "♥", "♦", "♣"]
-suits = ["S", "H", "D", "C"]
+suit_symbols = ["♠", "♥", "♣", "♦"]
+suits = ["S", "H", "C", "D"]
 numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 
